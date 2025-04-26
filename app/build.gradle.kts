49bec7ce39827cad0acc3a33f58f4fff2dfe2fef
Aboubacar Sidiki Camara <aboubacarsidikicamara@Aboubacar.local>
2025-04-26 19:18:23 -0400

    Ajout de la dépendance de la navigation entre les écrans

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,8 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
 
 
+    // La navigation
+    implementation(libs.androidx.navigation.compose)
     // Librairie du pdf
     implementation(libs.android.pdf.viewer)
 
