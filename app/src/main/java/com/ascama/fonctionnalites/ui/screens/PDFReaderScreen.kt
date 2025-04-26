023e21b456393899ccf2d4114e4c718bbc84b665
Aboubacar Sidiki Camara <aboubacarsidikicamara@Aboubacar.local>
2025-04-26 18:18:34 -0400

    Création du répertoire des écrans

---
+++ app/src/main/java/com/ascama/fonctionnalites/ui/screens/PDFReaderScreen.kt
@@ -0,0 +1,19 @@
+package com.ascama.fonctionnalites.ui.screens
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewFontScale
+import androidx.compose.ui.viewinterop.AndroidView
+
+@Composable
+fun PDFReaderScree() {
+
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun PDFReaderPreview() {
+    
+}
